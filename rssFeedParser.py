32bb76989f2ac4cdac6f6e7d0a417364f1499973
Adam Grabowski <48142646+Grabosky@users.noreply.github.com>
2019-03-12 22:41:30 -0400

    Create rssFeedParser.py

---
+++ rssFeedParser.py
@@ -0,0 +1,13 @@
+import feedparser
+
+d = feedparser.parse('http://www.somewebsiteaddress.com/rss')
+
+#Print feed titles
+print d['feed']['title']
+
+#Print feed content
+for post in d.entries:
+    print post.title + "\n" + post.description + "\n" + "Link: " + post.link + "\n"
+	
+	
+	
