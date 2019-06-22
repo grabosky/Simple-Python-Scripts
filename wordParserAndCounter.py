18f4c03673a00878f358b6c0e6535334840a0272
Adam Grabowski <48142646+grabosky@users.noreply.github.com>
2019-06-22 12:02:02 -0400

    Update wordParserAndCounter.py

--- wordParserAndCounter.py
+++ wordParserAndCounter.py
@@ -4,3 +4,4 @@ words = re.findall('\w+', open('hamlet.txt').read().lower())
 print collections.Counter(words)
 
 #this code opens hamlet.txt and prints the most commonly used words with number of used times
+
